216363beafc486c93ef7fcd63d38f605526673de
oritgurel <oritgurel@gmail.com>
2017-08-02 22:51:30 +0300

    Update helloWorld.java

--- helloWorld.java
+++ helloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
-public static void main(String args[]) {
-system.Out.println("Hello World");
-}
+	public static void main(String args[]) {
+		System.out.println("Hello World");
+	}
 }
